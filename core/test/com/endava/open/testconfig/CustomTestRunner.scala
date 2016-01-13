301f7c1bf5b8c980ae68c9ec8e5dff854576cd15
Elizabeta Ilievska <elizabeta.ilievska@endava.com>
2016-01-13 12:42:15 +0100

    Added dependencies for unit testing.

---
+++ core/test/com/endava/open/testconfig/CustomTestRunner.scala
@@ -0,0 +1,22 @@
+package com.endava.open.testconfig
+
+import com.badlogic.gdx.ApplicationListener
+
+/**
+  * Simple Empty class that extends ApplicationListener
+  * Only for the purpose of UnitSpec
+  */
+class CustomTestRunner extends ApplicationListener{
+
+  override def resize(width: Int, height: Int): Unit = {}
+
+  override def dispose(): Unit = {}
+
+  override def pause(): Unit = {}
+
+  override def render(): Unit = {}
+
+  override def resume(): Unit = {}
+
+  override def create(): Unit = {}
+}
