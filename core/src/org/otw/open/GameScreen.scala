a654064a335f4a6b444d5e1ee1a0aa6a91b93ed7
eilievska <elizabeta.ilievska@endava.com>
2016-01-18 15:53:07 +0100

    Removed sprite batch drawing.

--- core/src/org/otw/open/GameScreen.scala
+++ core/src/org/otw/open/GameScreen.scala
@@ -15,8 +15,5 @@ class GameScreen extends ScreenAdapter {
   override def render(delta: Float) = {
     Gdx.gl.glClearColor(1, 0, 0, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-    batch.begin()
-    batch.draw(img, 0, 0)
-    batch.end()
   }
 }
