2705651ab427badac49f70538c07dd3cd1e435ab
eilievska <elizabeta.ilievska@endava.com>
2016-01-25 10:44:31 +0100

    Desktop launcher setup

--- desktop/src/org/otw/open/desktop/DesktopLauncher.java
+++ desktop/src/org/otw/open/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import org.otw.open.OpenGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1440;
+        config.height = 900;
+        config.resizable = false;
+        config.title = "OPEN - a children's game";
         new LwjglApplication(new OpenGame(), config);
+
     }
 }
