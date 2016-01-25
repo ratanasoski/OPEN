6c6d1079aa923c879d60d35d1049c863a3d7e119
eilievska <elizabeta.ilievska@endava.com>
2016-01-25 15:13:20 +0100

    Created a companion object for the OpenGame class. Added tests.

--- core/src/org/otw/open/OpenGame.scala
+++ core/src/org/otw/open/OpenGame.scala
@@ -1,11 +1,34 @@
 package org.otw.open
 
-import com.badlogic.gdx.Game
+import com.badlogic.gdx.{Screen, Game}
 import org.otw.open.engine.impl.FirstGameScreenEngine
 
 /**
   * Created by eilievska on 1/13/2016.
   */
-class OpenGame extends Game {
+class OpenGame private() extends Game {
   override def create(): Unit = setScreen(new GameScreen(new FirstGameScreenEngine))
+}
+
+/**
+  * A singleton object containing an instance of our game.
+  */
+object OpenGame {
+  private lazy val game = new OpenGame
+
+  /**
+    *
+    * @return our Game instance.
+    */
+  def getGame: OpenGame = game
+
+  /**
+    * Sets a new screen to our game.
+    * @param newScreen -  a new screen for our game.
+    * @return the current (newly added) screen in the game.
+    */
+  def changeScreen(newScreen: Screen): Screen = {
+    game.setScreen(newScreen)
+    game.getScreen
+  }
 }
