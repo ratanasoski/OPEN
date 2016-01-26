2c4fe71ab46cec353f8a4549fe3099dad6dd4327
jivanovski <Jovan.Ivanovski@endava.com>
2016-01-26 14:15:06 +0100

    Animator class with tests

---
+++ core/src/org/otw/open/engine/util/Animator.scala
@@ -0,0 +1,27 @@
+package org.otw.open.engine.util
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.{Animation, TextureAtlas}
+
+/**
+  * Animator class that will handle all texture transitions for game animations
+  *
+  * Created by jivanovski on 1/25/2016.
+  */
+class Animator(val atlasFileName: String) {
+
+  /*global atlas*/
+  private val atlas = new TextureAtlas(Gdx.files.internal(atlasFileName))
+
+  /*animation setup*/
+  private val animation = new Animation(1 / 5f, atlas.getRegions)
+
+  /**
+    *
+    * @param timePassed time passed since animation started
+    * @return current texture region for the animation
+    */
+  def getCurrentTexture(timePassed: Float) = animation.getKeyFrame(timePassed, true)
+
+}
+
