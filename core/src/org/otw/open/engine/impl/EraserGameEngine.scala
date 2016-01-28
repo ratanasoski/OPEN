2032ae61ba1e7836b8139036d7c5620ad30b7c6d
deftimov <Daniel.Eftimov@endava.com>
2016-01-28 13:56:08 +0100

    Added unit tests for DrawablePixmap.scala and EraserGameEngine.scala Refactored  DrawablePixmap.scala and EraserGameEngine.scala to better Added missing dependency for ScalaMock in OPEN's Core module.

--- core/src/org/otw/open/engine/impl/EraserGameEngine.scala
+++ core/src/org/otw/open/engine/impl/EraserGameEngine.scala
@@ -10,35 +10,36 @@ import org.otw.open.{GameScreen, OpenGame}
 
 /**
   * Created by deftimov on 25.01.2016.
+  * An Engine that handles the erasing canvas games.
   */
 class EraserGameEngine extends InputAdapter with Engine {
   Gdx.input.setInputProcessor(this)
+  private val pixmapMask: DrawablePixmap = new DrawablePixmap(new Pixmap(Gdx.files.internal("leaves.png")))
+  private val backgroundTexture: Texture = new Texture(Gdx.files.internal("street.jpg"))
+  private val maskTexture: Texture = pixmapMask.initializePixmapDrawingOntoTexture
 
   private var lastPointerPosition: Option[Vector2] = None
-
-  private val pixmapMask: DrawablePixmap = new DrawablePixmap(new Pixmap(Gdx.files.internal("leaves.png")))
-  private val background: Texture = new Texture(Gdx.files.internal("street.jpg"))
-  private val foreground: Texture = pixmapMask.getMaskAsTexture
   private var mouseMoved = false
 
   Pixmap.setBlending(Blending.None)
 
+  /** @param delta time elapsed between frames
+    * @return List of drawings to be rendered
+    **/
   override def getDrawings(delta: Float): List[Drawing] = {
     if (mouseMoved) {
-      pixmapMask.changeTexture(foreground)
-      if (pixmapMask.isMaskFilled)
+      pixmapMask.drawOnTexture(maskTexture)
+      if (pixmapMask.isPixmapMaskTransparent)
         OpenGame.changeScreen(new GameScreen(new CauseAndEffectEngine(25 until 580, 85 until 377, List(new Vector2(0, 500), new Vector2(800, 500)))))
     }
-    List(new Drawing(background, 0, 0), new Drawing(foreground, 0, 0))
+    List(new Drawing(backgroundTexture, 0, 0), new Drawing(maskTexture, 0, 0))
   }
 
   /**
-    * returns true if the mouse was moved from one point to another,
-    * otherwise returns false if the mouse is idle
-    *
     * @param screenX x current coordinate of the pointer
     * @param screenY y current coordinate of the pointer
-    * @return true or false
+    * @return true if the mouse was moved from one point to another,
+    *         false otherwise if the mouse is idle
     */
   override def mouseMoved(screenX: Int, screenY: Int): Boolean = {
     val currentPosition: Vector2 = new Vector2(screenX, screenY)
@@ -62,7 +63,7 @@ class EraserGameEngine extends InputAdapter with Engine {
     **/
   override def dispose() = {
     pixmapMask.dispose
-    background.dispose
-    foreground.dispose
+    backgroundTexture.dispose
+    maskTexture.dispose
   }
 }
