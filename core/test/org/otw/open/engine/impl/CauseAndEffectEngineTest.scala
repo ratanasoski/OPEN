bd463c02d94995111d0bbaed92eb9b1f9ed991b3
smirakovska <Stefanija.Mirakovska@endava.com>
2016-01-29 16:32:49 +0100

    test fix for CauseAndEffectEngine

--- core/test/org/otw/open/engine/impl/CauseAndEffectEngineTest.scala
+++ core/test/org/otw/open/engine/impl/CauseAndEffectEngineTest.scala
@@ -1,13 +1,15 @@
 package org.otw.open.engine.impl
 
 import com.badlogic.gdx.math.{Vector2, Vector3}
-import org.otw.open.controllers.{CauseAndEffectFinishedSuccessfully, CauseAndEffectFinishedUnsuccessfully, ScreenController}
+import org.otw.open.controllers.{CauseAndEffectFinishedUnsuccessfully, ScreenController}
 import org.otw.open.dto.HorizontalMovingObject
 import org.otw.open.testconfig._
+import org.otw.open.{GameScreen, OpenGame}
+
 /**
   * Created by smirakovska on 1/26/2016.
   */
-class CauseAndEffectEngineTest extends UnitSpec{
+class CauseAndEffectEngineTest extends UnitSpec {
 
   val xRange: Range = 25 until 580
   val yRange: Range = 85 until 377
@@ -24,7 +26,7 @@ class CauseAndEffectEngineTest extends UnitSpec{
 
   test("when objectShouldStopAnimating is invoked it should return false for object not at the end point") {
     val testObject = new HorizontalMovingObject(0, 0, movementDelta)
-   assert(causeAndEffectEngine.objectShouldStopAnimating(testObject.x, testObject.y) == false)
+    assert(causeAndEffectEngine.objectShouldStopAnimating(testObject.x, testObject.y) == false)
   }
 
   test("when objectShouldStopAnimating is invoked it should return true for object at the end point") {
@@ -70,17 +72,29 @@ class CauseAndEffectEngineTest extends UnitSpec{
     assert(drawings.size == 2)
   }
 
-  test("when 3 failed attempts are made to click the object, screen with unhappy animation is shown"){
-    causeAndEffectEngine.touchDown(600,600,1,1)
-    causeAndEffectEngine.touchDown(600,600,1,1)
-    causeAndEffectEngine.touchDown(600,600,1,1)
-    assert(ScreenController.dispatchEvent(CauseAndEffectFinishedUnsuccessfully).isInstanceOf[StaticAnimationEngine] )
+  test("when 3 failed attempts are made to click the object, screen with unhappy animation is shown") {
+    causeAndEffectEngine.touchDown(600, 600, 1, 1)
+    causeAndEffectEngine.touchDown(600, 600, 1, 1)
+    causeAndEffectEngine.touchDown(600, 600, 1, 1)
+    assert(ScreenController.dispatchEvent(CauseAndEffectFinishedUnsuccessfully).isInstanceOf[StaticAnimationEngine])
   }
 
-  test("when object is clicked and animation is finished, screen with happy animation is shown"){
-    causeAndEffectEngine.touchDown(600,600,1,1)
-    causeAndEffectEngine.touchDown(600,600,1,1)
-    causeAndEffectEngine.touchDown(600,600,1,1)
-    assert(ScreenController.dispatchEvent(CauseAndEffectFinishedSuccessfully).isInstanceOf[StaticAnimationEngine] )
+  test("when object is clicked and animation is finished, screen with happy animation is shown") {
+    causeAndEffectEngine.touchDown(900, 900, 1, 1)
+    causeAndEffectEngine.touchDown(900, 900, 1, 1)
+    /**
+      * object is clicked
+      */
+    causeAndEffectEngine.touchDown(90, 90, 1, 1)
+
+    val gameScreen = OpenGame.getGame.getScreen match {
+      case s: GameScreen => s
+      case _ => throw new scala.ClassCastException
+    }
+    val staticAnimationEngine = gameScreen.engine match {
+      case sae: StaticAnimationEngine => sae
+      case _ => throw new scala.ClassCastException
+    }
+    assert(staticAnimationEngine.atlasFileName.endsWith("happy-animation.atlas"))
   }
 }
