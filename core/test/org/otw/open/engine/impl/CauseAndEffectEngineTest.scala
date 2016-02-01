76a370f562ec57101f0a53f8aacf1b9fb4bdb46b
smirakovska <Stefanija.Mirakovska@endava.com>
2016-02-01 12:45:30 +0100

    tests fix: override beforeEach()and afterEach() method

--- core/test/org/otw/open/engine/impl/CauseAndEffectEngineTest.scala
+++ core/test/org/otw/open/engine/impl/CauseAndEffectEngineTest.scala
@@ -1,27 +1,39 @@
 package org.otw.open.engine.impl
 
 import com.badlogic.gdx.math.{Vector2, Vector3}
+import org.junit.Before
 import org.otw.open.controllers.{CauseAndEffectFinishedUnsuccessfully, ScreenController}
 import org.otw.open.dto.HorizontalMovingObject
 import org.otw.open.testconfig._
 import org.otw.open.{GameScreen, OpenGame}
+import org.scalatest.BeforeAndAfterEach
 
 /**
   * Created by smirakovska on 1/26/2016.
   */
-class CauseAndEffectEngineTest extends UnitSpec {
+class CauseAndEffectEngineTest extends UnitSpec with BeforeAndAfterEach {
 
   val xRange: Range = 25 until 580
   val yRange: Range = 85 until 377
   val standPoints: List[Vector2] = List(new Vector2(0, 0), new Vector2(500, 0))
   val movementDelta: Int = 20
 
-  val causeAndEffectEngine: CauseAndEffectEngine = new CauseAndEffectEngine(xRange, yRange, standPoints)
+  var causeAndEffectEngine: CauseAndEffectEngine = _
 
   val transformator = new Function[Vector3, Vector2] {
     override def apply(vector: Vector3): Vector2 = {
       new Vector2(vector.x, vector.y)
     }
+  }
+
+
+  override def beforeEach(): Unit = {
+    causeAndEffectEngine = new CauseAndEffectEngine(xRange, yRange, standPoints)
+    causeAndEffectEngine.setMouseClickPositionTransformator(transformator)
+  }
+
+  override def afterEach(): Unit = {
+    causeAndEffectEngine.dispose()
   }
 
   test("when objectShouldStopAnimating is invoked it should return false for object not at the end point") {
@@ -40,23 +52,19 @@ class CauseAndEffectEngineTest extends UnitSpec {
   }
 
   test("when objectIsClicked is invoked should return false if object is not clicked") {
-    causeAndEffectEngine.setMouseClickPositionTransformator(transformator)
     assert(causeAndEffectEngine.objectIsClicked(50, 60) == false)
   }
 
 
   test("when objectIsClicked is invoked should return true if object is clicked") {
-    causeAndEffectEngine.setMouseClickPositionTransformator(transformator)
     assert(causeAndEffectEngine.objectIsClicked(90, 90) == true)
   }
 
   test("when mouse is clicked, should return true if object was clicked") {
-    causeAndEffectEngine.setMouseClickPositionTransformator(transformator)
     assert(causeAndEffectEngine.touchDown(95, 120, 1, 1) == true)
   }
 
   test("when mouse is clicked, should return false if object was not clicked") {
-    causeAndEffectEngine.setMouseClickPositionTransformator(transformator)
     assert(causeAndEffectEngine.touchDown(0, 900, 1, 1) == false)
   }
 
@@ -82,6 +90,7 @@ class CauseAndEffectEngineTest extends UnitSpec {
   test("when object is clicked and animation is finished, screen with happy animation is shown") {
     causeAndEffectEngine.touchDown(900, 900, 1, 1)
     causeAndEffectEngine.touchDown(900, 900, 1, 1)
+
     /**
       * object is clicked
       */
@@ -95,6 +104,7 @@ class CauseAndEffectEngineTest extends UnitSpec {
       case sae: StaticAnimationEngine => sae
       case _ => throw new scala.ClassCastException
     }
+
     assert(staticAnimationEngine.atlasFileName.endsWith("happy-animation.atlas"))
   }
 }
