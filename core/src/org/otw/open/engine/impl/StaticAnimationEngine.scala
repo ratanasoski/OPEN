9515914842b17133e86685545d5ab3ce5c5c8bef
smirakovska <Stefanija.Mirakovska@endava.com>
2016-02-04 16:00:51 +0100

    Implementation of reaching goal with multiple clicks: second level of game - model added - engine changes - test fixes

--- core/src/org/otw/open/engine/impl/StaticAnimationEngine.scala
+++ core/src/org/otw/open/engine/impl/StaticAnimationEngine.scala
@@ -1,8 +1,10 @@
 package org.otw.open.engine.impl
 
-import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.{InputAdapter, Gdx}
 import com.badlogic.gdx.graphics.Texture
-import org.otw.open.dto.Drawing
+import org.otw.open.controllers.{CauseAndEffectSecondLevel, ScreenController}
+import org.otw.open.dto.{StandPoint, Drawing}
 import org.otw.open.engine.Engine
 import org.otw.open.engine.util.Animator
 
@@ -10,7 +12,8 @@ import org.otw.open.engine.util.Animator
   * An engine class used for displaying animation that should be static and placed in the center of the screen.
   * Created by jivanovski on 1/25/2016.
   */
-class StaticAnimationEngine(val atlasFileName: String) extends Engine {
+class StaticAnimationEngine(val atlasFileName: String) extends InputAdapter with Engine {
+
   val animator = new Animator(atlasFileName)
   val background = new Texture(Gdx.files.internal("street.png"))
   private var timePassed = 0f
@@ -18,6 +21,18 @@ class StaticAnimationEngine(val atlasFileName: String) extends Engine {
   override def getDrawings(delta: Float): List[Drawing] = {
     timePassed = timePassed + delta
     List(new Drawing(background, 0, 0), new Drawing(animator.getCurrentTexture(timePassed), 464, 194))
+  }
+
+  /**
+    * this is an extra line and should be removed at end of sprint
+    */
+  Gdx.input.setInputProcessor(this)
+  /**
+    * should be removed at end of sprint
+    */
+  override def touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {
+    ScreenController.dispatchEvent(CauseAndEffectSecondLevel)
+    true
   }
 
   override def dispose(): Unit = {
